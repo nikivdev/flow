1d36d497a9e8a2c7bc9363cd9cbfa55eb9717518
Nikita <github@nikiv.dev>
2026-01-13 15:07:52 +0200

    Add fs plugin and rewrite flow logs with file-based streaming

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -167,6 +167,7 @@ fn main() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
